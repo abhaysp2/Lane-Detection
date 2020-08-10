36fd9de662dfa1398345b8180f54ab02604502c5
abhaysp2 <32227425+abhaysp2@users.noreply.github.com>
2020-08-10 11:46:24 -0500

    Add files via upload

---
+++ studentVision.py
@@ -0,0 +1,202 @@
+import time
+import math
+import numpy as np
+import cv2
+import rospy
+
+from line_fit import line_fit, tune_fit, bird_fit, final_viz
+from Line import Line
+from sensor_msgs.msg import Image
+from std_msgs.msg import Header
+from cv_bridge import CvBridge, CvBridgeError
+from std_msgs.msg import Float32
+from skimage import morphology
+
+
+
+class lanenet_detector():
+    def __init__(self):
+
+        self.bridge = CvBridge()
+        self.sub_image = rospy.Subscriber('camera/image_raw', Image, self.img_callback, queue_size=1)
+        self.pub_image = rospy.Publisher("lane_detection/annotate", Image, queue_size=1)
+        self.pub_bird = rospy.Publisher("lane_detection/birdseye", Image, queue_size=1)
+        self.left_line = Line(n=5)
+        self.right_line = Line(n=5)
+        self.detected = False
+        self.hist = True
+
+
+    def img_callback(self, data):
+
+        try:
+            # Convert a ROS image message into an OpenCV image
+            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            print(e)
+
+        raw_img = cv_image.copy()
+        mask_image, bird_image = self.detection(raw_img)
+
+        # Convert an OpenCV image into a ROS image message
+        out_img_msg = self.bridge.cv2_to_imgmsg(mask_image, 'bgr8')
+        out_bird_msg = self.bridge.cv2_to_imgmsg(bird_image, 'bgr8')
+
+        # Publish image message in ROS
+        self.pub_image.publish(out_img_msg)
+        self.pub_bird.publish(out_bird_msg)
+
+
+    def gradient_thresh(self, img, thresh_min=25, thresh_max=100):
+        """
+        Apply sobel edge detection on input image in x, y direction
+        """
+        #1. Convert the image to gray scale
+        #2. Gaussian blur the image
+        #3. Use cv2.Sobel() to find derievatives for both X and Y Axis
+        #4. Use cv2.addWeighted() to combine the results
+        #5. Convert each pixel to unint8, then apply threshold to get binary image
+
+        ## TODO
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        blur = cv2.GaussianBlur(gray,(25,25), 0)
+        sobelx = cv2.Sobel(blur, cv2.CV_8U, 1, 0, 17)
+        sobely = cv2.Sobel(blur, cv2.CV_8U, 0, 1, 17)
+        combined = np.uint8(cv2.addWeighted(sobelx, 0.7, sobely, 0.3, 0))
+        binary_output = (np.logical_and(combined>=thresh_min, combined<=thresh_max)).astype(int)
+        return binary_output
+
+
+    def color_thresh(self, img, thresh=(100, 255)):
+        """
+        Convert RGB to HSL and threshold to binary image using S channel
+        """
+        #1. Convert the image from RGB to HSL
+        #2. Apply threshold on S channel to get binary image
+
+        ## TODO
+        hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
+        white = (hls[:,:,1]>=240).astype(int)
+        temp_yellow = (np.logical_and(hls[:,:,0]>=20, hls[:,:,0]<=40)).astype(int)
+        yellow = np.logical_and(temp_yellow, hls[:,:,2]>=140).astype(int)
+        binary_output = np.logical_or(white, yellow).astype(int)
+        ####
+
+        return binary_output
+
+
+    def combinedBinaryImage(self, img):
+        """
+        Get combined binary image from color filter and sobel filter
+        """
+        #1. Apply sobel filter and color filter on input image
+        #2. Combine the outputs
+        ## Here you can use as many methods as you want.
+
+        ## TODO
+        #call the above two functions and decide how to combine the output arrays
+        SobelOutput = self.gradient_thresh(img)
+        ColorOutput = self.color_thresh(img)
+        ####
+
+        binaryImage = np.zeros_like(SobelOutput)
+        binaryImage[(ColorOutput==1)|(SobelOutput==1)] = 1
+        # Remove noise from binary image
+        binaryImage = morphology.remove_small_objects(binaryImage.astype('bool'),min_size=50,connectivity=2)
+
+
+        return binaryImage
+
+
+    def perspective_transform(self, img, verbose=False):
+        """
+        Get bird's eye view from input image
+        """
+        #1. Visually determine 4 source points and 4 destination points
+        #2. Get M, the transform matrix, and Minv, the inverse using cv2.getPerspectiveTransform()
+        #3. Generate warped image in bird view using cv2.warpPerspective()
+
+        ## TODO
+        image =np.array(img, dtype = np.uint8)
+        rect = np.array([[450,250],[740,250],[880,400],[120,400]], dtype="float32") #best
+        maxWidth = abs(rect[3][0]-rect[2][0])
+        maxHeight = abs(rect[0][1]-rect[2][1])
+        dest = np.array([[0,0],[maxWidth-1,0],[maxWidth-1,maxHeight-1],[0,maxHeight-1]], dtype="float32")
+        M = cv2.getPerspectiveTransform(rect,dest)
+        Minv = np.linalg.inv(M)
+        warped_img = cv2.warpPerspective(image,M,((maxWidth).astype(int),(maxHeight).astype(int)))
+        ####
+
+        return warped_img, M, Minv
+
+
+    def detection(self, img):
+
+        binary_img = self.combinedBinaryImage(img)
+        img_birdeye, M, Minv = self.perspective_transform(binary_img)
+
+        if not self.hist:
+            # Fit lane without previous result
+            ret = line_fit(img_birdeye)
+            left_fit = ret['left_fit']
+            right_fit = ret['right_fit']
+            nonzerox = ret['nonzerox']
+            nonzeroy = ret['nonzeroy']
+            left_lane_inds = ret['left_lane_inds']
+            right_lane_inds = ret['right_lane_inds']
+
+        else:
+            # Fit lane with previous result
+            if not self.detected:
+                ret = line_fit(img_birdeye)
+                left_fit = ret['left_fit']
+                right_fit = ret['right_fit']
+                nonzerox = ret['nonzerox']
+                nonzeroy = ret['nonzeroy']
+                left_lane_inds = ret['left_lane_inds']
+                right_lane_inds = ret['right_lane_inds']
+
+                left_fit = self.left_line.add_fit(left_fit)
+                right_fit = self.right_line.add_fit(right_fit)
+
+                self.detected = True
+
+            else:
+                left_fit = self.left_line.get_fit()
+                right_fit = self.right_line.get_fit()
+                ret = tune_fit(img_birdeye, left_fit, right_fit)
+                left_fit = ret['left_fit']
+                right_fit = ret['right_fit']
+                nonzerox = ret['nonzerox']
+                nonzeroy = ret['nonzeroy']
+                left_lane_inds = ret['left_lane_inds']
+                right_lane_inds = ret['right_lane_inds']
+
+                if ret is not None:
+                    left_fit = ret['left_fit']
+                    right_fit = ret['right_fit']
+                    nonzerox = ret['nonzerox']
+                    nonzeroy = ret['nonzeroy']
+                    left_lane_inds = ret['left_lane_inds']
+                    right_lane_inds = ret['right_lane_inds']
+
+                    left_fit = self.left_line.add_fit(left_fit)
+                    right_fit = self.right_line.add_fit(right_fit)
+
+                else:
+                    self.detected = False
+
+            # Annotate original image
+            bird_fit_img = bird_fit(img_birdeye, ret, save_file=None)
+            combine_fit_img = final_viz(img, left_fit, right_fit, Minv)
+
+
+            return combine_fit_img, bird_fit_img
+
+
+if __name__ == '__main__':
+    # init args
+    rospy.init_node('lanenet_node', anonymous=True)
+    lanenet_detector()
+    while not rospy.core.is_shutdown():
+        rospy.rostime.wallsleep(0.5)
